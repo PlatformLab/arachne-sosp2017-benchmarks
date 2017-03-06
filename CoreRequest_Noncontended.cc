337265e2d45d2c655220cba17e79bed20f1de695
Henry Qin <hq6@stanford.edu>
2017-03-06 00:26:13 -0800

    Updated benchmark to use new API call, but it looks like the amount of logging is slowing things down

--- CoreRequest_Noncontended.cc
+++ CoreRequest_Noncontended.cc
@@ -12,18 +12,15 @@ using CoreArbiter::CoreArbiterClient;
 
 #define NUM_TRIALS 100000
 
-std::atomic<bool> aboutToBlock(true);
 /**
   * This thread will get unblocked when a core is allocated, and will block
   * itself again when the number of cores is decreased.
   */
 void coreExec(CoreArbiterClient& client) {
     for (int i = 0; i < NUM_TRIALS; i++) {
-        aboutToBlock = true;
         TimeTrace::record("Core thread about to block");
         client.blockUntilCoreAvailable();
         TimeTrace::record("Core thread returned from block");
-        aboutToBlock = false;
         while (!client.shouldReleaseCore());
         TimeTrace::record("Core informed that it should block");
     }
@@ -37,11 +34,12 @@ int main(){
     std::vector<core_t> oneCoreRequest = {1,0,0,0,0,0,0,0};
     std::vector<core_t> zeroCoreRequest = {0,0,0,0,0,0,0,0};
     for (int i = 0; i < NUM_TRIALS; i++) {
-        // Release a core and wait for release
-        while (!aboutToBlock);
+        // When the number of blocked threads becomes nonzero, we request a core.
+        while (client.getNumBlockedThreads() == 0);
         TimeTrace::record("Requesting a core");
         client.setNumCores(oneCoreRequest);
-        while (aboutToBlock);
+        // When the number of blocked threads becomes zero, we release a core.
+        while (client.getNumBlockedThreads() == 1);
         TimeTrace::record("Releasing a core");
         client.setNumCores(zeroCoreRequest);
     }
