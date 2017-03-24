412c70d1998110f73443ddf075736a4679d06b49
Henry Qin <hq6@stanford.edu>
2017-03-24 11:28:13 -0700

    Initial implementation of synthetic workload
    Currently there are no means for external controls. This benchmark demonstrates
    that the current load tracking in Arachne doesn't ramp up fast enough or at
    all.

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
-LIBS=-ICoreArbiter CoreArbiter/libCoreArbiter.a -IPerfUtils PerfUtils/libPerfUtils.a -pthread
+LIBS=-ICoreArbiter -IArachne -IPerfUtils PerfUtils/libPerfUtils.a -pthread
 CXXFLAGS=-std=c++11 -O3 -Wall -Werror -Wformat=2 -Wextra -Wwrite-strings -Wno-unused-parameter -Wmissing-format-attribute -Wno-non-template-friend -Woverloaded-virtual -Wcast-qual -Wcast-align -Wconversion -fomit-frame-pointer
 
 ARBITER_BENCHMARK_BINS = CoreRequest_Noncontended
+UNIFIED_BENCHMARK_BINS = SyntheticWorkload
 
-all: $(ARBITER_BENCHMARK_BINS)
+all: $(ARBITER_BENCHMARK_BINS) $(UNIFIED_BENCHMARK_BINS)
 
 $(ARBITER_BENCHMARK_BINS) : % : %.cc CoreArbiter/libCoreArbiter.a
-	g++  $(DEBUG) $(CXXFLAGS)  $< $(LIBS) -o $@
+	g++  $(DEBUG) $(CXXFLAGS)  $^ $(LIBS) -o $@
+
+$(UNIFIED_BENCHMARK_BINS): % : %.cc Arachne/libArachne.a CoreArbiter/libCoreArbiter.a
+	g++  $(DEBUG) $(CXXFLAGS)  $^ $(LIBS) -o $@
 
 clean:
-	rm -f $(ARBITER_BENCHMARK_BINS) *.log
+	rm -f $(ARBITER_BENCHMARK_BINS) $(UNIFIED_BENCHMARK_BINS) *.log
---
+++ SyntheticWorkload.cc
@@ -0,0 +1,82 @@
+#include <random>
+#include <thread>
+#include "Arachne.h"
+#include "PerfUtils/Cycles.h"
+
+using PerfUtils::Cycles;
+
+std::atomic<uint64_t> completions;
+void fixedWork(uint64_t duration) {
+	Cycles::sleep(duration);
+    completions++;
+}
+
+void dispatch() {
+	Arachne::makeExclusiveOnCore();
+    // Start with a DCFT-style implementation based on shared-memory for communication
+    // If that becomes too expensive, then switch to a separate thread for commands.
+
+    // Note that this controlling thread will necessarily burn up an entire core, since
+    // it has to poll to see whether it should issue more requests as well as
+    // polling on shared memory for commands.
+
+    // TODO: Use shared memory to expose these controls.
+
+    // Could it be that the right way to do this is to simply provide Arachne
+    // with the equivalent of Go's LockOSThread? To get an exclusive core for
+    // this function alone? That way, Arachne can work with such threads and
+    // schedule other threads as needed.
+    // Generate time in ns because that's more intuitive for users and 
+	// Must be floating point type...
+	double creationsPerSecond = 1000000U;
+	uint64_t durationPerThread = 0;
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+    std::exponential_distribution<double> intervalGenerator(creationsPerSecond);
+    printf("%.10f %.10f\n", 1.0/creationsPerSecond, intervalGenerator(gen));
+    uint64_t nextCycleTime = Cycles::rdtsc() +
+		Cycles::fromSeconds(intervalGenerator(gen));
+
+    uint64_t currentTime = Cycles::rdtsc();
+    uint64_t finalTime = currentTime + Cycles::fromSeconds(1);
+    // DCFT loop
+    uint64_t failureRate = 0;
+    for (; currentTime < finalTime ; currentTime = Cycles::rdtsc()) {
+        if (nextCycleTime < currentTime) {
+            nextCycleTime = currentTime +
+                Cycles::fromSeconds(intervalGenerator(gen));
+            if (Arachne::createThread(fixedWork, durationPerThread) == Arachne::NullThread)
+                failureRate++;
+        }
+    }
+    printf("Completions %lu\n"
+           "Failed Creations %lu\n", completions.load(), failureRate);
+    fflush(stdout);
+    Arachne::makeSharedOnCore();
+    Arachne::shutDown();
+}
+
+/**
+ * This synthetic benchmarking tool allows us to create threads at a Poisson
+ * arrival rate with a configurable mean. These threads run for a configurable
+ * amount of time. We record the times that configuration changes go into
+ * effect, so we can plot them later against the effects that these changes
+ * caused.
+ *
+ * The inter-arrival time of a Poisson distribution with mean λ is given
+ * by an exponential distribution with mean (1/λ), so we use an exponential
+ * distribution from the c++ standard library.
+ *
+ * Note that we should probably bechmark the cost of extracting randomness as
+ * well, but we haven't yet done that.
+ */
+
+int main() {
+	Arachne::numCores = 4;
+	Arachne::maxNumCores = 4;
+    Arachne::init();
+    Arachne::createThread(dispatch);
+    Arachne::waitForTermination();
+}
