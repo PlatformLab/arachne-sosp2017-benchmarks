777e9fb8581cf4ba14821f30d4c16f947dd51f6b
jspeiser <jspeiser80@gmail.com>
2017-04-04 18:30:22 -0700

    Buggy cooperative preemption benchmark

--- CoreRequest_Contended.cc
+++ CoreRequest_Contended.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <thread>
 #include <atomic>
+#include <sys/wait.h>
 
 #include "CoreArbiterClient.h"
 #include "Logger.h"
@@ -16,25 +17,33 @@ using namespace CoreArbiter;
 #define NUM_TRIALS 1000
 
 void highPriorityRequest(CoreArbiterClient& client,
-                         volatile bool* lowPriorityRunning) {
+                         std::atomic<bool>* lowPriorityRunning) {
     client.blockUntilCoreAvailable();
 
     // Wait until the other high priority thread is running
     while (client.getNumOwnedCores() < 2);
 
     for (int i = 0; i < NUM_TRIALS; i++) {
-        TimeTrace::record("About to request fewer cores");
+        LOG(ERROR, "*** %d ***\n", i);
+        LOG(ERROR, "About to request fewer cores\n");
         client.setNumCores({1,0,0,0,0,0,0,0});
-        TimeTrace::record("Requested fewer cores");
+        LOG(ERROR, "Requested fewer cores\n");
         while (client.getNumBlockedThreadsFromServer() == 0);
-        TimeTrace::record("High priority thread blocked.");
+        LOG(ERROR, "High priority thread blocked\n");
         while (!(*lowPriorityRunning));
-        TimeTrace::record("About to request more cores");
+        LOG(ERROR, "About to request more cores\n");
         client.setNumCores({2,0,0,0,0,0,0,0});
-        TimeTrace::record("Requested more cores");
-        while(client.getNumBlockedThreads() == 1);
+        LOG(ERROR, "Requested more cores\n");
+        uint32_t numBlockedThreads;
+        while(*(lowPriorityRunning));
+        do {
+            numBlockedThreads = client.getNumBlockedThreads();
+        } while(numBlockedThreads == 1);
+        LOG(ERROR, "numBlockedThreads = %u\n", numBlockedThreads);
+        // while(client.getNumBlockedThreads() == 1);
     }
 
+    LOG(ERROR, "******************\n");
     client.unregisterThread();
 }
 
@@ -45,53 +54,63 @@ void highPriorityBlock(CoreArbiterClient& client) {
     while (client.getNumOwnedCores() < 2);
 
     for (int i = 0; i < NUM_TRIALS; i++) {
+        LOG(ERROR, "!!! %d !!!\n", i);
         while (!client.mustReleaseCore());
-        TimeTrace::record("High priority core release requested.");
+        LOG(ERROR, "High priority core release requested\n");
         client.blockUntilCoreAvailable();
-        TimeTrace::record("High priority core acquired.");
+        LOG(ERROR, "High priority core acquired.\n");
     }
 
+    LOG(ERROR, "!!!!!!!!!!!!!!!\n");
     client.unregisterThread();
 }
 
 void lowPriorityExec(CoreArbiterClient& client,
-                     volatile bool* lowPriorityRunning) {
+                     std::atomic<bool>* lowPriorityRunning) {
     std::vector<uint32_t> lowPriorityRequest = {0,0,0,0,0,0,0,1};
     client.setNumCores(lowPriorityRequest);
     client.blockUntilCoreAvailable();
 
+    *lowPriorityRunning = true;
+
     // Wait for other process to join
-    while (client.getNumProcessesOnServer() == 1);
+    // while (client.getNumProcessesOnServer() == 1);
 
     for (int i = 0; i < NUM_TRIALS; i++) {
+        LOG(ERROR, "### %d ###\n", i);
         while (!client.mustReleaseCore());
-        TimeTrace::record("Low priority core release requested");
+        LOG(ERROR, "Low priority core release requested\n");
         *lowPriorityRunning = false;
         client.blockUntilCoreAvailable();
-        TimeTrace::record("Low priority core acquired");
+        LOG(ERROR, "Low priority core acquired\n");
         *lowPriorityRunning = true;
     }
 
+    while (!client.mustReleaseCore());
+    LOG(ERROR, "Low priority core release requested\n");
+    *lowPriorityRunning = false;
+
+    LOG(ERROR, "################\n");
     client.unregisterThread();
 }
 
 int main(){
-    Logger::setLogLevel(ERROR);
+    Logger::setLogLevel(NOTICE);
 
     int sharedMemFd = open("benchmark_sharedmem",
                            O_CREAT | O_RDWR | O_TRUNC, S_IRWXU);
     if (sharedMemFd < 0) {
-        fprintf(stderr, "Error opening shared memory page: %s\n",
+        LOG(ERROR, "Error opening shared memory page: %s\n",
                 strerror(errno));
         return -1;
     }
 
-    ftruncate(sharedMemFd, sizeof(bool));
-    volatile bool* lowPriorityRunning = (bool*)mmap(NULL, getpagesize(),
+    ftruncate(sharedMemFd, sizeof(std::atomic<bool>));
+    std::atomic<bool>* lowPriorityRunning = (std::atomic<bool>*)mmap(NULL, getpagesize(),
                                            PROT_READ | PROT_WRITE, MAP_SHARED,
                                            sharedMemFd, 0);
     if (lowPriorityRunning == MAP_FAILED) {
-        fprintf(stderr, "Error on global stats mmap: %s\n", strerror(errno));
+        LOG(ERROR, "Error on global stats mmap: %s\n", strerror(errno));
         return -1;
     }
     *lowPriorityRunning = false;
@@ -110,17 +129,23 @@ int main(){
                                         lowPriorityRunning);
         highPriorityThread1.join();
         highPriorityThread2.join();
+        client.unregisterThread();
 
-        TimeTrace::setOutputFileName("CoreRequest_Contended_HighPriority.log");
-        TimeTrace::print();
+        LOG(ERROR, "@@@@@@@ AFTER JOIN @@@@@@@@@@@\n");
+
+        // TimeTrace::setOutputFileName("CoreRequest_Contended_HighPriority.log");
+        // TimeTrace::print();
     } else  {
         CoreArbiterClient& client =
             CoreArbiterClient::getInstance("/tmp/CoreArbiter/testsocket");
         lowPriorityExec(client, lowPriorityRunning);
 
-        TimeTrace::setOutputFileName("CoreRequest_Contended_LowPriority.log");
-        TimeTrace::print();
+        // TimeTrace::setOutputFileName("CoreRequest_Contended_LowPriority.log");
+        // TimeTrace::print();
 
-        wait();
+        if (wait(NULL) < 0) {
+            LOG(ERROR, "Error on wait: %s\n", strerror(errno));
+        }
+
     }
 }
