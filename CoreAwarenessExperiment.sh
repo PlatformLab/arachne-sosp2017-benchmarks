450df0f7e23a55f0b8b34754f9438c41eae96994
Henry Qin <hq6@stanford.edu>
2017-04-09 17:47:50 -0700

    Modified the core awarenesss experiment to generate a grid instead

--- CoreAwarenessExperiment.sh
+++ CoreAwarenessExperiment.sh
@@ -3,16 +3,15 @@
 # Vary the number of cores with a fixed number of kernel threads (the correct
 # number 5, due to one dispatch and 4 workers)
 # For both, we have report one less becaues the dispatch thread takes one.
-echo Cores,Kernel Threads,Duration,Offered Load,Core Utilization,Median Latency,99% Latency,Throughput
-for i in 2 3 4 5 6 7; do 
-echo -n $((i - 1)),4,
-./VaryCoreAndKThread.sh $i 5 | tail -n 1
-sleep 1
-done
 
-# Vary the number of kernel threads with a fixed number of cores
+CORE_RANGE="2 3 4 5 6 7"
+KERNEL_THREAD_RANGE="2 3 4 5 6 7"
+
 echo Cores,Kernel Threads,Duration,Offered Load,Core Utilization,Median Latency,99% Latency,Throughput
-for i in 2 3 4 5 6 7; do 
-echo -n 4,$((i - 1)),
-./VaryCoreAndKThread.sh 5 $i | tail -n 1
+for i in $CORE_RANGE; do
+  for j in 2 3 4 5 6 7; do
+    echo -n $((i - 1)),$((j - 1)),
+    ./VaryCoreAndKThread.sh $i $j | tail -n 1
+    sleep 1
+  done
 done
